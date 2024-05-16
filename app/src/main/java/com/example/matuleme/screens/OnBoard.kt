3b69fc6ee615421c3005c0f84910473d285baaf6
MKudryash <82795441+MKudryash@users.noreply.github.com>
2024-05-16 20:05:43 +0300

    feat: +анимация в OnBoard

--- app/src/main/java/com/example/matuleme/screens/OnBoard.kt
+++ app/src/main/java/com/example/matuleme/screens/OnBoard.kt
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,30 +17,43 @@ import com.example.matuleme.databinding.ActivityOnBoardBinding
 import com.example.matuleme.databinding.ActivitySplashBinding
 import com.example.matuleme.models.OnBoardModel
 import com.example.matuleme.objects.PrefManager
+import io.paperdb.Paper
 import java.util.LinkedList
 import java.util.Queue
 
-class OnBoard : CustomActivity() {
+class OnBoard : CustomActivity(), AnimationListener {
     private lateinit var binding: ActivityOnBoardBinding
     private var queue: Queue<OnBoardModel> = LinkedList()
+    private lateinit var animIn: Animation
+    private lateinit var animOut: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        animIn = AnimationUtils.loadAnimation(this, R.anim.alpha_in)
+        animOut = AnimationUtils.loadAnimation(this, R.anim.alpha_out)
+        animIn.setAnimationListener(this)
         createQueue()
         pressButton()
     }
 
-    private fun createQueue(){
-        queue = LinkedList(
-            listOf(
-                OnBoardModel("ДОБРО\nПОЖАЛОВАТЬ", "", R.drawable.on_board1, R.drawable.on_board1_b, "Начать", 1),
-                OnBoardModel("Начнем\nпутешествие", "Умная, великолепная и модная коллекция Изучите сейчас", R.drawable.on_board2, R.drawable.on_board2_b, "Далее", 2),
-                OnBoardModel("У Вас Есть Сила,\nЧтобы", "В вашей комнате много красивых и привлекательных растений", R.drawable.on_board3, R.drawable.on_board3_b, "Далее", 3)
+    private fun createQueue() {
+        queue = Paper.book().read<LinkedList<OnBoardModel>>("queue",
+            LinkedList(
+                listOf(
+                    OnBoardModel("ДОБРО\nПОЖАЛОВАТЬ", "", R.drawable.on_board1, R.drawable.on_board1_b, "Начать", 1),
+                    OnBoardModel("Начнем\nпутешествие", "Умная, великолепная и модная коллекция Изучите сейчас", R.drawable.on_board2, R.drawable.on_board2_b, "Далее", 2),
+                    OnBoardModel("У Вас Есть Сила,\nЧтобы", "В вашей комнате много красивых и привлекательных растений", R.drawable.on_board3, R.drawable.on_board3_b, "Далее", 3)
+                )
             )
-        )
+        )!!
+        saveStateQueue()
         enterQueue(queue.poll()!!)
+    }
+
+    private fun saveStateQueue(){
+        Paper.book().write("queue", queue)
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
@@ -96,10 +112,33 @@ class OnBoard : CustomActivity() {
                     finish()
                 }
                 else {
-                    enterQueue(queue.poll()!!)
+                    if(queue.size != 1) {
+                        txtStartTittle.startAnimation(animIn)
+                    }
+                    img.startAnimation(animIn)
+                    txtNextTittle.startAnimation(animIn)
+                    txtDesc.startAnimation(animIn)
                 }
             }
         }
+    }
+
+    override fun onAnimationStart(animation: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+        with(binding){
+            saveStateQueue()
+            enterQueue(queue.poll()!!)
+            img.startAnimation(animOut)
+            txtNextTittle.startAnimation(animOut)
+            txtDesc.startAnimation(animOut)
+        }
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) {
+
     }
 
 }
